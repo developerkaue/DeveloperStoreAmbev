4de58510c23819e3223387ff935e74e41c093a59
kaue <kauecaobiancofernandes2@gmail.com>
2025-02-03 11:05:39 -0300

    Adding libs and packages to use:
    EntityFrameworkCore
    EntityFrameworkCore.Design
    EntityFrameworkCore.Npgsql
    EntityFrameworkCore.Tools
    
    MediatR
    FluentValidation
    AutoMapper.Extensions.Microsoft.DependencyInjection
    xunit
    Moq
    FluentAssertions

--- DeveloperStoreAmbev/DeveloperEvaluation.UnitTests/obj/Debug/net8.0/DeveloperEvaluation.UnitTests.AssemblyInfo.cs
+++ DeveloperStoreAmbev/DeveloperEvaluation.UnitTests/obj/Debug/net8.0/DeveloperEvaluation.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeveloperEvaluation.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd4725d59b4497f3e20a498423c01fef2052a97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd0bca823175c0cc08e52b1c4b98fdc319dc8f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeveloperEvaluation.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeveloperEvaluation.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
