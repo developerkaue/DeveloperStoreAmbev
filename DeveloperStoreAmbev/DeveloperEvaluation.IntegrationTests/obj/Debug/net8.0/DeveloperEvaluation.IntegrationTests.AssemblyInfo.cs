d549b9974d98174fd7b558731ef30a453a179945
kaue <kauecaobiancofernandes2@gmail.com>
2025-02-04 11:54:54 -0300

    bugfix: solve erros in pipeline tests integration

--- DeveloperStoreAmbev/DeveloperEvaluation.IntegrationTests/obj/Debug/net8.0/DeveloperEvaluation.IntegrationTests.AssemblyInfo.cs
+++ DeveloperStoreAmbev/DeveloperEvaluation.IntegrationTests/obj/Debug/net8.0/DeveloperEvaluation.IntegrationTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeveloperEvaluation.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3dcf9f70ec34df26293d0747a7640c3d1ddac80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+432892b83ba0b439b9f1b66753d35d913de3f1ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeveloperEvaluation.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeveloperEvaluation.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
