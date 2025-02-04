c77da702773f3ee06f91aa80f663070510f7077b
kaue <kauecaobiancofernandes2@gmail.com>
2025-02-04 11:24:52 -0300

    feature: add ci-cd-pipeline gitflow

--- DeveloperStoreAmbev/DeveloperEvaluation.UnitTests/obj/Debug/net9.0/DeveloperEvaluation.UnitTests.AssemblyInfo.cs
+++ DeveloperStoreAmbev/DeveloperEvaluation.UnitTests/obj/Debug/net9.0/DeveloperEvaluation.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeveloperEvaluation.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b4e6e971cb50d9175e3d16465c45e8c0f518292")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3dcf9f70ec34df26293d0747a7640c3d1ddac80")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeveloperEvaluation.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeveloperEvaluation.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
